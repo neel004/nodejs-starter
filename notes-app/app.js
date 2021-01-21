9d19aee3603777f43946edb9938e2ccc91a3dc04
neel004 <47408929+neel004@users.noreply.github.com>
2021-01-21 23:19:59 +0530

    completed notes making app using node.js
    first app using node.js.

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,12 +1,70 @@
 // const validator = require('validator');
-const read_notes = require('./notes.js');
+const yargs = require('yargs')
+const notes = require('./notes.js');
 const chalk = require('chalk')
+// console.log(process.argv)
+// customise yargs version
+    yargs.version('1.1.1')
+    //create add command
+    yargs.command({
+        command : 'add',
+        builder : {
+            body:{
+                describe : 'Note Body',
+                demandOption : true,
+                type : 'string'
+            },
+            title : {
+                describe : 'Note title',
+                demandOption : true,
+                type : 'string'
+            }
+        },
+        describe : 'Add a new note',
+        handler(argv){
+            notes.addNote(argv.title,argv.body)
+        }
+    })
+    //create remove command
+    yargs.command({
+        command : 'remove',
+        builder :{
+            title : {
+                describe : 'Note title',
+                demandOption : true,
+                type : 'string'
+            }
+        },
+        describe : 'remove note',
+        handler(argv){
+            notes.removeNote(argv.title)
+        }
+    })
 
-const err = chalk.red.bold
-const success = chalk.green.underline.dim.inverse
-const warning = chalk.keyword('orange')
-const filename = "notes.txt";
-read_notes(filename);
-// console.log(validator.isURL('google.com'))
-console.log(chalk.blue.bold('Hello world!'))
-console.log(err("404")+success("neel")+warning("alert"))
+    //creating read command
+    yargs.command({
+        command : 'read',
+        builder : {
+            title :{
+                describe : 'Note title',
+                demandOption : true,
+                type : 'string'
+            }
+        },
+        describe : 'read note',
+        handler(argv){
+            notes.readNotes(argv.title)
+        }
+    })
+
+    //creating list command 
+    yargs.command({
+        command: 'list',
+        describe : 'list all notes',
+        handler(){
+            notes.getAllNotes()
+        }
+    })
+// add ,remove ,read ,list
+// console.log(yargs.argv)
+yargs.parse()
--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,12 +1,63 @@
 const fs = require('fs');
 
-const read = function (a){
-    console.log(fs.readFileSync(a, encoding= "UTF8"));
+
+const loadNotes = () =>{
+    try{
+        const data_buffer =  fs.readFileSync('notes.json')
+        const j_data_buffer = data_buffer.toString()
+        return JSON.parse(j_data_buffer)
+    }catch(e){
+        return []
+    }
 }
 
-const write = function(a,txt){
-    fs.appendFileSync(a,txt);
+const readNotes = (title) => {
+    const notes = loadNotes()
+    const find_index = notes.findIndex((note)=>note.title === title)
+    if(typeof(notes[find_index])==='undefined'){
+        console.log('No Note Found With Provided Title')
+    }else{
+        console.log(notes[find_index])
+    }
+}
+
+const addNotes = (title,body) => {
+    const notes = loadNotes()
+    const dup_notes = notes.filter((note)=>note.title === title)
+    debugger //node inspect command.....
+    if(dup_notes.length === 0 ){
+        notes.push({
+            title: title,
+            body : body
+        })
+        console.log('Note Added')
+        saveNotes(notes)
+    }else{
+        console.log('Note title taken')
+    }
+   
+}
+
+const saveNotes =  (notes)=>{
+    const j_data = JSON.stringify(notes)
+    fs.writeFileSync('notes.json',j_kdata)
 }
 //write('new.txt',"First note, second note");
 //read("new.txt");
-module.exports = read ;
+const removeNote = (title) => {
+    const notes = loadNotes()
+    // const find_index = notes.findIndex(x=>x.title === title)
+    // notes.splice(find_index,1)
+    const updated_notes = notes.filter((notes)=>notes.title != title)
+    saveNotes(updated_notes)
+}
+const getAllNotes = ()=>{
+    const notes = loadNotes()
+    notes.forEach((note) =>console.log(note.title));
+}
+module.exports = {
+    readNotes : readNotes,//read
+    addNote : addNotes,   //write
+    removeNote : removeNote,//remove
+    getAllNotes : getAllNotes//list
+}
