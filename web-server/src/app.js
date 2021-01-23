7dc194e995ef5d6943e8c674dfe9a326d5ceae97
neel004 <47408929+neel004@users.noreply.github.com>
2021-01-24 01:22:48 +0530

    updated look of website
    applied little bit of css.

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,35 +1,40 @@
 const path = require('path')
+const hbs = require('hbs')
 const express = require('express')
 
 const app = express()
 
 // Define paths for express config
 const public_dir_path = path.join(__dirname,'../public') //pointing to public content
-const viewsPath = path.join(__dirname,'../views') //custom views path
+const viewsPath = path.join(__dirname,'../templets/views') //custom views path
+const partialsPath = path.join(__dirname,'../templets/partials')
+
 //setup static dir. to serve
 app.use(express.static(public_dir_path))
+
 //setup handlebar engin and views location 
 app.set('views' , viewsPath)
 app.set('view engine','hbs')
+hbs.registerPartials(partialsPath)
 
 
 app.get('',(req,res)=>{
     res.render('index',{
         title : 'Weather',
-        name : 'Neel'
+        name : ' Neel Patel'
     })
 })
 app.get('/about',(req,res)=>{
     res.render('about',{
         title : 'About Me',
-        name : 'Neel'
+        name : ' Neel Patel'
     })
 })
 
 app.get('/help' , (req,res)=>{
     res.render('help',{
         title : 'Help',
-        email : '18it092@charusat.edu.in'
+        name : ' Neel Patel'
     })
 })
 app.get('/weather',(req,res)=>{
@@ -38,6 +43,20 @@ app.get('/weather',(req,res)=>{
         latitude :1,
         temperature : 0.13
     }])
+})
+app.get('/help/*',(req,res)=>{
+    res.render('404',{
+        title : '404',
+        name : 'Neel Patel',
+        error : 'Help Article not found'
+    })
+})
+app.get('*',(req,res)=>{
+    res.render('404',{
+        title : '404',
+        name : 'Neel Patel',
+        error : 'Page Not found'
+    })
 })
 app.listen(3000,()=>{
     console.log('Server is up on port 3000')
